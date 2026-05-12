8633e02b6b7806a9ca3fef68f27c274d711204d8
Eric Tran <eric.tran@ysocorp.com>
2026-05-12 17:31:39 +0200

    upd: PhysicMaterialCombine 2 = Minimum | 3 = Multiply

--- bin/js_modules/OimoPhysics.js
+++ bin/js_modules/OimoPhysics.js
@@ -21047,11 +21047,11 @@ oimo.dynamics.constraint.contact.ContactConstraint = class oimo_dynamics_constra
 			case 0: // Average
 				usedFriction = (this._s1._friction + this._s2._friction) / 2;
 				break;
-			case 1: // Minimum
-				usedFriction = this._s1._friction < this._s2._friction ? this._s1._friction : this._s2._friction;
-				break;
-			case 2: // Multiply
+			case 1: // Multiply
 				usedFriction = this._s1._friction * this._s2._friction;
+				break;
+			case 2: // Minimum
+				usedFriction = this._s1._friction < this._s2._friction ? this._s1._friction : this._s2._friction;
 				break;
 			case 3: // Maximum
 				usedFriction = this._s1._friction > this._s2._friction ? this._s1._friction : this._s2._friction;
@@ -21064,10 +21064,10 @@ oimo.dynamics.constraint.contact.ContactConstraint = class oimo_dynamics_constra
 				usedRestitution = (this._s1._restitution + this._s2._restitution) / 2;
 				break;
 			case 1:
-				usedRestitution = this._s1._restitution < this._s2._restitution ? this._s1._restitution : this._s2._restitution;
+				usedRestitution = this._s1._restitution * this._s2._restitution;
 				break;
 			case 2:
-				usedRestitution = this._s1._restitution * this._s2._restitution;
+				usedRestitution = this._s1._restitution < this._s2._restitution ? this._s1._restitution : this._s2._restitution;
 				break;
 			case 3:
 				usedRestitution = this._s1._restitution > this._s2._restitution ? this._s1._restitution : this._s2._restitution;
